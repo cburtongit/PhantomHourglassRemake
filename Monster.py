aa62cbcb113962b67e8a5884964765b5fbddba3b
C.J.Burton <cb883@kent.ac.uk>
2020-03-02 13:55:28 +0000

    Initial commit. Don't know how to use GitHub

---
+++ Monster.py
@@ -0,0 +1,33 @@
+import pygame
+import Entity
+
+
+class Monster(Entity):
+
+    # battle stats
+    # health - total health, once 0 death() should be called
+    health = 0
+    # atk - amount of damage done to target
+    atk = 0
+    windUp = 0
+
+    # what items the enemy has, should only have 1 or 2 unless boss, similar to player.inventory[]
+    equipped = []
+
+    # loyalty determines interaction with other Entities - 0 - player; 1 - phantom; 2 - neutral;
+    loyalty = 0
+
+    # hostile to the player or player-loyal Entities - 0 - not hostile; 1 - hostile to player ONLY; 2 - hostile to all;
+    hostile = 0
+
+    # movement variables
+    # moveUp = moveRight = moveDown = moveLeft = False
+    direction = ''
+
+    def __init__(self, health, atk, windUp, inv, drc, x, y, e):
+        super().__init__(x, y, e)
+        self.health = health
+        self.atk = atk
+        self.windUp = windUp
+        self.inventory = inv
+        self.direction = drc
